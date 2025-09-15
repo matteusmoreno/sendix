920002f24fb4027ae6d18f999411f4e971550bed
Matteus Moreno <matteus_moreno@live.com>
2025-09-15 18:10:11 -0300

    first commit

---
+++ src/test/java/com/matteusmoreno/sendix/SendixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matteusmoreno.sendix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
