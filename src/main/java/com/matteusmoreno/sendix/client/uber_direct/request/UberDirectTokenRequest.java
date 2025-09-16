0a9199ec975d42992e4d8ae71358ad53c8ab23a3
Matteus Moreno <matteus_moreno@live.com>
2025-09-15 22:00:16 -0300

    add anotations to UberDirectTokenRequest

--- src/main/java/com/matteusmoreno/sendix/client/uber_direct/request/UberDirectTokenRequest.java
+++ src/main/java/com/matteusmoreno/sendix/client/uber_direct/request/UberDirectTokenRequest.java
@@ -1,9 +1,15 @@
 package com.matteusmoreno.sendix.client.uber_direct.request;
 
 import feign.form.FormProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UberDirectTokenRequest {
     @FormProperty("client_id")
     private String clientId;
